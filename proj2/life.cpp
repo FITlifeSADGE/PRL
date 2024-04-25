79b6986c0a8ab401bf01924704027a94690402a9
FITlifeSADGE <lukas8967@gmail.com>
2024-04-25 16:45:38 +0200

    Finished proj 2

--- proj2/life.cpp
+++ proj2/life.cpp
@@ -5,8 +5,9 @@
 #include <cmath>
 #include <queue>
 
+// Funkce změní buňku podle pravidel
 std::string change_cell(const char &cell, std::vector<char> &neighbours) {
-if (cell == '0') {
+if (cell == '0') { // pokud je buňka mrtvá, ožije pouze pokud má 3 živé sousedy
     int alive_neighbours = 0;
     for (char c : neighbours) {
         if (c == '1') {
@@ -14,14 +15,14 @@ if (cell == '0') {
         }
     }
     if (alive_neighbours == 3) {
-        return "1";
+        return "1"; // tady ožije
     }
     else {
-        return "0";
+        return "0"; // jinak zůstává mrtvá
     }
 
 }
-else {
+else { // pokud je buňka živá, zůstává žít, pokud má živé právě 2 nebo 3 sousedy
     int alive_neighbours = 0;
     for (char c : neighbours) {
         if (c == '1') {
@@ -29,24 +30,25 @@ else {
         }
     }
     if (alive_neighbours == 2 || alive_neighbours == 3) {
-        return "1";
+        return "1"; // tady zůstává žít
     }
     else {
-        return "0";
+        return "0"; // jinak umírá
     }
 }
 }
 
-
+// Funkce zjistí sousední buňky pro každou buňku v řádku
 std::string get_neighbours(const std::string &line, const std::string &neighbour1, const std::string &neighbour2) {
     std::vector<char> neighbours;
     std::string final = "";
-    for (int i = 0; i < line.size(); ++i) {
+    for (int i = 0; i < line.size(); ++i) { // projdu všechny buňky v řádku
         neighbours.clear();
 
-        int neighbour_left = (i == 0) ? line.size() - 1 : i - 1; // vezmu prvek nalevo
-        int neighbour_right = (i == line.size() - 1) ? 0 : i + 1; // vezmu prvek napravo
+        int neighbour_left = (i == 0) ? line.size() - 1 : i - 1; // vezmu prvek nalevo (pokud jsem první prvek, beru poslední)
+        int neighbour_right = (i == line.size() - 1) ? 0 : i + 1; // vezmu prvek napravo (pokud jsem poslední prvek, beru první)
 
+        // do pole sousedů přidávám všechny sousedy buňky
         neighbours.push_back(neighbour1[neighbour_left]);
         neighbours.push_back(neighbour1[i]);
         neighbours.push_back(neighbour1[neighbour_right]);
@@ -55,22 +57,23 @@ std::string get_neighbours(const std::string &line, const std::string &neighbour
         neighbours.push_back(neighbour2[neighbour_right]);
         neighbours.push_back(line[neighbour_left]);
         neighbours.push_back(line[neighbour_right]);
-
+        // do nového řádku přidám upravenou buňku
         final += change_cell(line[i], neighbours);
     }
     return final;
 }
 
+// funkce na inicializační odeslání řádků
 void send_to_others(const std::string &line, int comm_rank, int comm_size) {
-    if (comm_rank == 0) {
+    if (comm_rank == 0) { // první procesor pošle druhému a poslednímu
         MPI_Send(line.c_str(), line.size(), MPI_CHAR, comm_size - 1, 0, MPI_COMM_WORLD);
         MPI_Send(line.c_str(), line.size(), MPI_CHAR, comm_rank + 1, 1, MPI_COMM_WORLD);      
     }
-    if (comm_rank == comm_size - 1) {
+    if (comm_rank == comm_size - 1) { // poslední procesor pošle prvnímu a předchozímu
         MPI_Send(line.c_str(), line.size(), MPI_CHAR, comm_rank - 1, 0, MPI_COMM_WORLD);
         MPI_Send(line.c_str(), line.size(), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
     }
-    if (comm_rank != 0 && comm_rank != comm_size - 1) {
+    if (comm_rank != 0 && comm_rank != comm_size - 1) { // ostatní pošlou sousedům
         MPI_Send(line.c_str(), line.size(), MPI_CHAR, comm_rank - 1, 0, MPI_COMM_WORLD);
         MPI_Send(line.c_str(), line.size(), MPI_CHAR, comm_rank + 1, 1, MPI_COMM_WORLD);
     }
@@ -84,51 +87,53 @@ void recv_from_others(int comm_rank, int comm_size, const std::string &line, int
     std::string new_line = line;
 
     for (int i = 0; i < iterations; i++) {
-        if (comm_rank == 0) {
+        if (comm_rank == 0) { // první procesor dostane od posledního a druhého
             MPI_Recv(recv_line, line_size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
             recv_line[line_size] = '\0';
 
             MPI_Recv(recv_line2, line_size, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
             recv_line2[line_size] = '\0';
-            new_line = get_neighbours(new_line, recv_line, recv_line2);
-            if (i + 1 < iterations) {
+            new_line = get_neighbours(new_line, recv_line, recv_line2); // zjistí svůj řádek po jedné iteraci
+            if (i + 1 < iterations) { // opět svůj nový řádek pošle sousedům
                 MPI_Send(new_line.c_str(), new_line.size(), MPI_CHAR, comm_size - 1, 0, MPI_COMM_WORLD);
                 MPI_Send(new_line.c_str(), new_line.size(), MPI_CHAR, comm_rank + 1, 1, MPI_COMM_WORLD);
             }
         }
-        if (comm_rank == comm_size - 1) {
+        if (comm_rank == comm_size - 1) { // poslední procesor dostane od prvního a předposledního
             MPI_Recv(recv_line, line_size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
             recv_line[line_size] = '\0';
 
             MPI_Recv(recv_line2, line_size, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
             recv_line2[line_size] = '\0';
-            new_line = get_neighbours(new_line, recv_line, recv_line2);
-            if (i + 1 < iterations) {
+            new_line = get_neighbours(new_line, recv_line, recv_line2); // zjistí svůj řádek po jedné iteraci
+            if (i + 1 < iterations) { // opět svůj nový řádek pošle prvnímu a předposlednímu
                 MPI_Send(new_line.c_str(), new_line.size(), MPI_CHAR, comm_rank - 1, 0, MPI_COMM_WORLD);
                 MPI_Send(new_line.c_str(), new_line.size(), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
             }
         }
-        if (comm_rank != 0 && comm_rank != comm_size - 1) {
+        if (comm_rank != 0 && comm_rank != comm_size - 1) { // ostatní procesory dostanou od sousedů
             MPI_Recv(recv_line, line_size, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
             recv_line[line_size] = '\0';
 
             MPI_Recv(recv_line2, line_size, MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
             recv_line2[line_size] = '\0';
-            new_line = get_neighbours(new_line, recv_line, recv_line2);
-            if (i + 1 < iterations) {      
+            new_line = get_neighbours(new_line, recv_line, recv_line2); // zjistí svůj řádek po jedné iteraci
+            if (i + 1 < iterations) {   // opět svůj nový řádek pošle sousedům
                 MPI_Send(new_line.c_str(), new_line.size(), MPI_CHAR, comm_rank - 1, 0, MPI_COMM_WORLD);
                 MPI_Send(new_line.c_str(), new_line.size(), MPI_CHAR, comm_rank + 1, 1, MPI_COMM_WORLD);
             }
         }
     }
-    if (comm_rank == 0) {
+    // finální přidání řádků do pole
+    if (comm_rank == 0) { // první procesor dostane řádky od ostatních
         for (int src = 1; src < comm_size; ++src) {
             MPI_Recv(recv_line, line_size, MPI_CHAR, src, 9999-src, MPI_COMM_WORLD, &status);
             recv_line[line_size] = '\0';
-            all_lines[src] = recv_line;
+            all_lines[src] = recv_line; // uloží řádek do pole
         }
-        all_lines[0] = new_line;
+        all_lines[0] = new_line; // první řádek uloží na první místo
     }
+    // ostatní procesory pošlou svůj řádek
     if (comm_rank != 0) {
         MPI_Send(new_line.c_str(), new_line.size(), MPI_CHAR, 0, 9999-comm_rank, MPI_COMM_WORLD);
     }
@@ -160,13 +165,12 @@ int main(int argc, char *argv[]) {
         std::cerr << "Něco se pokazilo" << std::endl;
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
-    std::string *all_lines = nullptr;
-    if (comm_rank == 0) {
-        all_lines = new std::string[comm_size];
-    }
+    // vytvořím pole pro všechny řádky
+    std::string *all_lines = new std::string[comm_size];
     
+    // po dokončení funkce mám všechny řádky v poli all_lines
     recv_from_others(comm_rank, comm_size, line, iterations, all_lines);
-    if (comm_rank == 0) {
+    if (comm_rank == 0) { // procesor s rankem 0 vypíše všechny řádky
         for (int i = 0; i < comm_size; ++i) {
             std::cout << i << ": " << all_lines[i] << std::endl;
         }
